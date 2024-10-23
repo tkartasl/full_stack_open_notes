aa7f9673da903b36e2997c7b28dfc1bcfb0bf9e6
Tomi Kartaslammi <tomi.kartaslammi@gmail.com>
2024-10-23 11:39:52 +0300

    Deleted 0,0,0,0 port

--- notebackend/index.js
+++ notebackend/index.js
@@ -90,6 +90,6 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, '0,0,0,0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
