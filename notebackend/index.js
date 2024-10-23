598721bbaf2e863a21fefdc1615b1b446c49f819
Tomi Kartaslammi <tomi.kartaslammi@gmail.com>
2024-10-23 11:34:56 +0300

    Added 0,0,0,0 to ports to listen

--- notebackend/index.js
+++ notebackend/index.js
@@ -90,6 +90,6 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+app.listen(PORT, '0,0,0,0', () => {
   console.log(`Server running on port ${PORT}`)
 })
